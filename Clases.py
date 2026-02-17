e867c1537e3c15b9ee2ae66a71d0b9ddda84912f
Carlos Abel <aguado170305@gmail.com>
2026-02-17 16:43:14 -0500

    dia7

---
+++ Clases.py
@@ -0,0 +1,22 @@
+class Pajaro:
+    
+    def __init__(self, color, especie):
+        self.color = color
+        self.especie = especie
+
+    def piar(self):
+        print('pio')
+
+    def volar(self, metros):
+        print(f'El pajaro ha volado {metros} metros')
+
+
+piolin = Pajaro('amarillo', 'canario') 
+
+piolin.piar()
+piolin.volar(100)
+
+
+# mi_pajaro = Pajaro('negro')
+
+# print(mi_pajaro.color)
